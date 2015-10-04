4a61fc622ce422eec1723ddf214acfd297971b73
Bob van Schendel <bobyantius@hotmail.com>
2015-10-04 12:15:57 +0200

    made the smiley assignment and actually committed it

---
+++ Assignment 6/Assignment 6/Smiley.py
@@ -0,0 +1,19 @@
+import math
+
+inputnum = int(raw_input("input the size of the smiley you wish to draw, at smaller sizes it may be extremely ugly: "))+1
+outputstr = ""
+radius = inputnum/2
+for y in range(inputnum):
+    for x in range(inputnum):
+        ydist = abs (y - int(radius))
+        xdist = abs (x - int(radius))
+        if int(math.ceil(math.sqrt(ydist**2 + xdist**2))) == radius:
+            outputstr += "*"
+        elif y == int(inputnum/3) and (x == int(inputnum/3) or x == int(inputnum/3 *2)):
+            outputstr += "*"
+        elif y > int(inputnum / 3 * 2)and int(math.floor(math.sqrt(ydist**2 + xdist**2))) == int(radius/2):
+            outputstr += "*"
+        else:
+            outputstr += " "
+    outputstr+="\n"
+print outputstr
