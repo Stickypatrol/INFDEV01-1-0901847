34a1eb35875a567cdc5504e1f60e6ffc42e52416
Bob van Schendel <bobyantius@hotmail.com>
2015-10-06 12:39:24 +0200

    made assignment 6

--- Assignment 6/Assignment 6/fullcircle.py
+++ Assignment 6/Assignment 6/fullcircle.py
@@ -1,4 +1,4 @@
-import math
+﻿import math
 
 inputnum = int(raw_input("input the size of the circleyou wish to be drawn: "))
 outputstr = ""
@@ -7,7 +7,7 @@ for y in range(inputnum+2):
     for x in range(inputnum):
         ydist = abs (y - int(radius))
         xdist = abs (x - int(radius))
-        if int(math.ceil(math.sqrt(ydist**2 + xdist**2))) < radius:
+        if int(math.sqrt(ydist**2 + xdist**2)) < radius:
             outputstr += "*"
         else:
             outputstr += " "
