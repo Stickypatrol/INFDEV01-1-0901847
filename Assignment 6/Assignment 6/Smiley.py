3b704862ef8a56bf14b51d14ee613c6f10f08a4c
Bob van Schendel <bobyantius@hotmail.com>
2015-10-06 12:20:42 +0200

    finished the assignments

--- Assignment 6/Assignment 6/Smiley.py
+++ Assignment 6/Assignment 6/Smiley.py
@@ -7,11 +7,11 @@ for y in range(inputnum):
     for x in range(inputnum):
         ydist = abs (y - int(radius))
         xdist = abs (x - int(radius))
-        if int(math.ceil(math.sqrt(ydist**2 + xdist**2))) == radius:
+        if int(math.sqrt(ydist**2 + xdist**2)) == radius:
             outputstr += "*"
         elif y == int(inputnum/3) and (x == int(inputnum/3) or x == int(inputnum/3 *2)):
             outputstr += "*"
-        elif y > int(inputnum / 3 * 2)and int(math.floor(math.sqrt(ydist**2 + xdist**2))) == int(radius/2):
+        elif y > int(inputnum / 3 * 2)and int(math.sqrt(ydist**2 + xdist**2)) == int(radius/2):
             outputstr += "*"
         elif y > int(inputnum / 4 * 1.75) and y < int(inputnum / 4 * 2.25) and x == int(inputnum/2):
             outputstr += "*"
