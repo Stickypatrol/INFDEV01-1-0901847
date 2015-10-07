0178a7aebfe7d4c710e743f67ef2c86273d1c689
Bob van Schendel <bobyantius@hotmail.com>
2015-10-07 11:37:39 +0200

    updated isosceles triangle

--- Assignment 6/Assignment 6/isosceles triangle.py
+++ Assignment 6/Assignment 6/isosceles triangle.py
@@ -1,11 +1,9 @@
-inputnum = int(raw_input("input the size of the triangle you wish to be drawn: "))
+﻿inputnum = int(raw_input("input the size of the triangle you wish to be drawn: "))
 outputstr = ""
 for i in range(inputnum):
     for p in range(inputnum + i):
         if p < inputnum-1-i:
             outputstr += " "
-        elif p > inputnum-1:
-            outputstr += "*"
         else:
             outputstr += "*"
     outputstr += "\n"
